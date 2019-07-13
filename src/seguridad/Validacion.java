8bc981fb5dd640210b3d16dd154950e917c7e2dd
DOCENTE - Jorge Alfredo Bojorquez Segura <c17428@utp.edu.pe>
2019-07-13 17:12:47 -0500

    Cambios en clase validacion v2 JB

--- src/seguridad/Validacion.java
+++ src/seguridad/Validacion.java
@@ -4,9 +4,9 @@ public class Validacion {
     public String validaUsuario(String usuario){
         String msg = "";
         msg = "esta correcto el usuario "
-                + "este es un mensaje "
-                + "de prueba previo a  "
-                + "cambiar a una mejor  "
+                + "este es un mensaje nuevo zzzzzz aaaaa"
+                + "de prueba previo a  dddddd"
+                + "cambiar a una mejor ewewe "
                 + "contrasena en menos "
                 + "de 90 dias";
         return msg;
