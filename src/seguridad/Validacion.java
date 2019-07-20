16b71f995e1301fca6225b044f08165b72f1831c
Jorge B <bojorquez.ja@gmail.com>
2019-07-20 15:28:10 -0500

    cambios finales

--- src/seguridad/Validacion.java
+++ src/seguridad/Validacion.java
@@ -4,7 +4,7 @@ public class Validacion {
     public String validaUsuario(String usuario){
         String msg = "";
         msg = "esta correcto el usuario "
-                + "este es un mensaje nuevo zzzzzz aaaaa"
+                + "CAMBIOS mensaje nuevo zzzzzz aaaaa"
                 + "de prueba previo a  dddddd"
                 + "cambiar a una mejor ewewe "
                 + "contrasena en menos "
