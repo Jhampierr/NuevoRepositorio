e180d1ce3f9436535d2f0bad8a8e7bc672f0cc65
Jorge B <bojorquez.ja@gmail.com>
2019-07-20 15:32:49 -0500

    cambios finales 4

--- src/seguridad/Validacion.java
+++ src/seguridad/Validacion.java
@@ -3,7 +3,8 @@ package seguridad;
 public class Validacion {
     public String validaUsuario(String usuario){
         String msg = "";
-        msg = "esta correcto el usuario "
+        msg = "esta correcto el usuario que cambia"
+                + "otra lina de codigo"
                 + "este es CAMBIO mensaje nuevo zzzzzz aaaaa"
                 + "de prueba previo a  dddddd"
                 + "cambiar a una mejor ewewe "
